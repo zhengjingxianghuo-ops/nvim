55a66433aa82d5f750027723795f77ba4c09c758
xuwenxiang <zhengjingxianghuo@gmail.com>
2025-12-16 21:41:39 +0800

    second commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,12 @@
+local opts = { noremap = true, silent = true, buffer = bufnr }
+
 vim.keymap.set('i', 'jk', '<Esc>', { desc = '退出插入模式', noremap = true, silent = true })
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,19 +1,26 @@
-vim.lsp.enable 'html'
-vim.lsp.enable 'cssls'
-vim.lsp.enable 'ts_ls'
-vim.lsp.enable 'lua_ls'
-vim.lsp.enable 'rust_anlyzer'
+local clients = {
+  vim.lsp.enable('html'),
+  vim.lsp.enable('cssls'),
+  vim.lsp.enable('ts_ls'),
+  vim.lsp.enable('lua_ls'),
+  vim.lsp.enable('clangd'),
+  vim.lsp.enable('rust_anlyzer'),
+}
+
+for _, client in ipairs(clients) do
+  if client then
+    client.on_attach = on_attach
+  end
+end
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function()
-    vim.lsp.buf.format({
-      async = false,
-    })
+    vim.lsp.buf.format({ async = false })
   end,
 })
 
 vim.diagnostic.config({
   signs = false,
   underline = false,
-  virtual_text = true
+  virtual_text = true,
 })
